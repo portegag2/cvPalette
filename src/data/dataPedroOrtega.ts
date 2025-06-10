af45892ae553fbd999aad9ebbc97db92b897c86c
portegag2 <portegag2@gmail.com>
2025-06-10 13:57:24 +0200

    Mock Data

---
+++ src/data/dataPedroOrtega.ts
@@ -0,0 +1,77 @@
+
+export const pedroData = {
+  "datos_personales": {
+    "nombre": "Pedro Ortega Goitia",
+    "rol_profesional": "Desarrollador Web Full Stack",
+    "fecha_nacimiento": "21/05/1971",
+    "ciudad": "Bilbao",
+    "telefono": "620 527 022",
+    "email": "portegag2@gmail.com",
+    "foto": "fotoCvPedroOrtega.png"
+  },
+  "perfil_profesional": "Ingeniero informático, 25 años desarrollando software, haciendo análisis y reingeniería tanto en proyectos de grandes cuentas, como Iberdrola (SIC/DELTA), Banesto, Gobierno Vasco, con roles específicos de análisis/desarrollo o en start-ups de plataformas web con necesidades globales, Full Stack. Dando mucha importancia al cuidado del trabajo en equipo.",
+  "experiencia_laboral": [
+    {
+      "titulo": "Analista Programador",
+      "entidad": "Iberdrola - Accenture",
+      "fecha": "12/2019 – 11/2024",
+      "descripcion": "Trabajando para Accenture en el proyecto SIC/DELTA de Iberdrola en la parte de NATStar, FrontEnd, en funciones de analista programador."
+    },
+    {
+      "titulo": "Analista Programador Full Stack – PHP, MySQL",
+      "entidad": "PLANNAM – SEMITAM INVESTMENTS",
+      "fecha": "07/2017 – 12/2019",
+      "descripcion": "Desarrollo Full Stack del aplicativo Plannam, análisis y desarrollo de reingeniería, así como optimizaciones de rendimiento y nuevas funcionalidades."
+    },
+    {
+      "titulo": "Analista Programador Full Stack – PHP, MySQL",
+      "entidad": "Autónomo – B2B Automoción",
+      "fecha": "07/2013 – 07/2017",
+      "descripcion": "Desarrollo Full-Stack en la creación y mantenimiento de los portales www.buscotaller.es y www.buscorecambios.es."
+    },
+    {
+      "titulo": "Analista Programador Full Stack– PHP, MySQL",
+      "entidad": "uvedoblestudio",
+      "fecha": "08/2008 – 05/2013",
+      "descripcion": "Reingeniería en las herramientas de la empresa y posterior desarrollo de nuevas utilidades. Una de ellas un foro de conocimiento para Osakidetza. Otra un SIP para mejorar los pasos en la obtención de sellos de calidad gestionados por Euskalit y Bikain. Y otra para realizar encuestas especializada en centros educativos."
+    },
+    {
+      "titulo": "Analista Programador – PHP, JSP, MySQL",
+      "entidad": "Autónomo, Altia, Laboradomo, Irontec",
+      "fecha": "06/2000 – 04/2001 y 02/2004 – 06/2008",
+      "descripcion": "Desarrollo Full-Stack y mantenimiento de un portal web para una federación deportiva y extrapolarlo a otro más genérico para competiciones, así como otro portal de búsquedas inmobiliarias. Adaptación de paquetes de software libre: ecommerce, CRM, Wordpress, Foro, Wiki. Creación de informes técnicos para cumplir la LOPD."
+    },
+    {
+      "titulo": "Técnico de sistemas",
+      "entidad": "Estudio Arquitectura San Andrés",
+      "fecha": "10/2001 – 11/2003",
+      "descripcion": "Aprovisionamiento, instalación y mantenimiento de material informático y telefonía. Desarrollo de BD para la gestión de horas (Access)."
+    },
+    {
+      "titulo": "Programador Senior – NATStar, SQL Windows",
+      "entidad": "Ceinsa, Ibermática, Bilbomática",
+      "fecha": "06/1997 – 06/2000",
+      "descripcion": "Programador Senior cedido en los clientes Banesto (NATStar), Iberdrola (NATStar) y Gobierno Vasco (SQLWindows)."
+    }
+  ],
+  "formacion": [
+    {
+      "titulo": "Ingeniero en Informática",
+      "entidad": "Universidad de Deusto",
+      "fecha": "10/1989 – 09/1995",
+      "descripcion": "Máster en Ingeniería del Software"
+    }
+  ],
+  "habilidades_tecnicas": [
+    {
+      "lenguaje_programacion": "PHP, MySQL, NATStar, HTML, JavaScript, Java, CSS",
+      "herramientas": "Eclipse, Visual Studio Code, Git"
+    }
+  ],
+  "idiomas": [
+    {
+      "idioma": "Inglés",
+      "nivel": "Alto en lectura, conversación y escrito"
+    }
+  ]
+};
--- src/data/sampleData.ts
+++ src/data/sampleData.ts
@@ -1,63 +1,63 @@
 
 export const pedroData = {
   "datos_personales": {
-    "nombre": "Pedro Ortega Goitia",
+    "nombre": "Julio Iglesias García",
     "rol_profesional": "Desarrollador Web Full Stack",
-    "fecha_nacimiento": "21/05/1971",
+    "fecha_nacimiento": "10/10/1999",
     "ciudad": "Bilbao",
-    "telefono": "620 527 022",
-    "email": "portegag2@gmail.com",
+    "telefono": "620 345 698",
+    "email": "iglesitas@gmail.com",
     "foto": "fotoCvPedroOrtega.png"
   },
-  "perfil_profesional": "Ingeniero informático, 25 años desarrollando software, haciendo análisis y reingeniería tanto en proyectos de grandes cuentas, como Iberdrola (SIC/DELTA), Banesto, Gobierno Vasco, con roles específicos de análisis/desarrollo o en start-ups de plataformas web con necesidades globales, Full Stack. Dando mucha importancia al cuidado del trabajo en equipo.",
+  "perfil_profesional": "Con el espíritu de un artesano digital y la visión de un arquitecto, soy un desarrollador web **full stack** que no solo construye sitios y aplicaciones, sino que también esculpe experiencias interactivas y robustas desde los cimientos hasta el último píxel. Mi dominio se extiende por todo el espectro del desarrollo web, donde el **front-end** cobra vida con interfaces intuitivas y estéticamente impecables que deleitan al usuario, y el **back-end** se erige como una fortaleza de lógica empresarial sólida, bases de datos eficientes y APIs perfectamente orquestadas.",
   "experiencia_laboral": [
     {
       "titulo": "Analista Programador",
-      "entidad": "Iberdrola - Accenture",
+      "entidad": "Coca-cola - Pepsico",
       "fecha": "12/2019 – 11/2024",
-      "descripcion": "Trabajando para Accenture en el proyecto SIC/DELTA de Iberdrola en la parte de NATStar, FrontEnd, en funciones de analista programador."
+      "descripcion": "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex. Adipiscing elit quisque faucibus ex sapien vitae pellentesque."
     },
     {
       "titulo": "Analista Programador Full Stack – PHP, MySQL",
-      "entidad": "PLANNAM – SEMITAM INVESTMENTS",
+      "entidad": "betaRet",
       "fecha": "07/2017 – 12/2019",
-      "descripcion": "Desarrollo Full Stack del aplicativo Plannam, análisis y desarrollo de reingeniería, así como optimizaciones de rendimiento y nuevas funcionalidades."
+      "descripcion": "Lorem ipsum dolor sit amet consectetur adipiscing elit. Dolor sit amet consectetur adipiscing elit quisque faucibus."
     },
     {
       "titulo": "Analista Programador Full Stack – PHP, MySQL",
-      "entidad": "Autónomo – B2B Automoción",
+      "entidad": "Govierno Vasco",
       "fecha": "07/2013 – 07/2017",
-      "descripcion": "Desarrollo Full-Stack en la creación y mantenimiento de los portales www.buscotaller.es y www.buscorecambios.es."
+      "descripcion": "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet consectetur adipiscing elit quisque faucibus ex."
     },
     {
       "titulo": "Analista Programador Full Stack– PHP, MySQL",
-      "entidad": "uvedoblestudio",
+      "entidad": "Studio Web",
       "fecha": "08/2008 – 05/2013",
-      "descripcion": "Reingeniería en las herramientas de la empresa y posterior desarrollo de nuevas utilidades. Una de ellas un foro de conocimiento para Osakidetza. Otra un SIP para mejorar los pasos en la obtención de sellos de calidad gestionados por Euskalit y Bikain. Y otra para realizar encuestas especializada en centros educativos."
+      "descripcion": "Lorem ipsum dolor sit amet consectetur adipiscing elit. Sit amet. Adipiscing elit quisque faucibus ex sapien vitae pellentesque."
     },
     {
       "titulo": "Analista Programador – PHP, JSP, MySQL",
-      "entidad": "Autónomo, Altia, Laboradomo, Irontec",
+      "entidad": "Autónomo, Interino",
       "fecha": "06/2000 – 04/2001 y 02/2004 – 06/2008",
-      "descripcion": "Desarrollo Full-Stack y mantenimiento de un portal web para una federación deportiva y extrapolarlo a otro más genérico para competiciones, así como otro portal de búsquedas inmobiliarias. Adaptación de paquetes de software libre: ecommerce, CRM, Wordpress, Foro, Wiki. Creación de informes técnicos para cumplir la LOPD."
+      "descripcion": "Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu."
     },
     {
       "titulo": "Técnico de sistemas",
-      "entidad": "Estudio Arquitectura San Andrés",
+      "entidad": "Abogados Asociados",
       "fecha": "10/2001 – 11/2003",
-      "descripcion": "Aprovisionamiento, instalación y mantenimiento de material informático y telefonía. Desarrollo de BD para la gestión de horas (Access)."
+      "descripcion": "Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
     },
     {
       "titulo": "Programador Senior – NATStar, SQL Windows",
-      "entidad": "Ceinsa, Ibermática, Bilbomática",
+      "entidad": "Iberdrola, Gobierno Vasco",
       "fecha": "06/1997 – 06/2000",
-      "descripcion": "Programador Senior cedido en los clientes Banesto (NATStar), Iberdrola (NATStar) y Gobierno Vasco (SQLWindows)."
+      "descripcion": "Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor."
     }
   ],
   "formacion": [
     {
-      "titulo": "Ingeniero en Informática",
-      "entidad": "Universidad de Deusto",
+      "titulo": "Ingeniero de caminos y veredas",
+      "entidad": "Universidad de Xixon",
       "fecha": "10/1989 – 09/1995",
       "descripcion": "Máster en Ingeniería del Software"
     }
